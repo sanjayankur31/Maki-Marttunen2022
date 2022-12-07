aa8b3d8f622751018e16c7cfe1ca1de2878942b7
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-12-07 19:01:20 +0000

    chore: add a test dir

---
+++ NeuroML/HayEtAlL5PYR/cellmodel/tests/Readme.md
@@ -0,0 +1,1 @@
+This directory contains a test hoc file that loads the original NEURON model to extract some information from it.
---
+++ NeuroML/HayEtAlL5PYR/cellmodel/tests/getinfo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Get info from NEURON model
+
+File: NEURON/getinfo.py 
+
+Copyright 2022 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+import json
+import yaml
+
+from pyneuroml.neuron import morphinfo, getinfo, load_hoc_or_python_file
+from neuron import h
+
+
+load_hoc_or_python_file("test_L5PC.hoc")
+
+with open("NEURON-morphinfo.yaml", "w") as f:
+    retval = morphinfo()
+    print(yaml.dump(retval, sort_keys=True, indent=4), file=f, flush=True)
+with open("NEURON-info.yaml", "w") as f:
+    retval = getinfo(h.allsec())
+    print(yaml.dump(retval, sort_keys=True, indent=4), file=f, flush=True)
