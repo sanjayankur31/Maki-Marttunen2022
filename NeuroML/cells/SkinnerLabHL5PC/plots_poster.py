2528339f94c01898d1c099eee9a4fa5d243140a2
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-07-12 13:36:47 +0100

    feat: add scripts to plot morph for poster

--- NeuroML/cells/SkinnerLabHL5PC/plots_poster.py
+++ NeuroML/cells/SkinnerLabHL5PC/plots_poster.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 """
-Enter one line description here.
+Flip cell to make it upright
 
-File:
+File: flip.py
 
 Copyright 2023 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
+
+import math
+import neuroml
+from neuroml.loaders import read_neuroml2_file
+from neuroml.writers import NeuroMLWriter
+try:
+    from pyneuroml.utils import rotate_cell
+except ImportError:
+    pass
+from pyneuroml.plot.PlotMorphology import plot_2D
+
 import matplotlib
 from matplotlib import pyplot as plt
-from pyneuroml.plot.PlotMorphology import plot_2D
 
 matplotlib.rcParams['font.weight'] = "bold"
 matplotlib.rcParams['axes.labelweight'] = "bold"
@@ -18,10 +28,14 @@ matplotlib.rcParams['axes.titlesize'] = "large"
 matplotlib.rcParams['axes.titleweight'] = "bold"
 
 
-inputfile = "L5PC.morph.cell.nml"
-title = "Rodent\n(Hay)"
+inputfile = "HL5PC.cell.nml"
+title = "Human\n(Rich)"
 plane = "xy"
-plot_2D(inputfile, min_width=6, nogui=True, plane2d=plane,
+nml_doc = read_neuroml2_file("HL5PC.cell.nml")
+thecell = nml_doc.cells[0]
+rotated_cell = rotate_cell(thecell, x=0, y=0, z=math.pi, relative_to_soma=True)
+
+plot_2D(rotated_cell, min_width=6, nogui=True, plane2d=plane,
         close_plot=False, title=title, square=False)
 fig = plt.gcf()
 plt.title(title, fontsize=18)
