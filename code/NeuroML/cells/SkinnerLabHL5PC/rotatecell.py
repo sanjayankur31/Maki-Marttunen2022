202223efb6bbc4efd4496fa235951f54018eb4a5
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-10-10 16:40:42 +0100

    feat(Skinnerlab-cell): rotate to orient correctly

---
+++ code/NeuroML/cells/SkinnerLabHL5PC/rotatecell.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Enter one line description here.
+
+File:
+
+Copyright 2023 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+from pyneuroml.pynml import read_neuroml2_file, write_neuroml2_file
+from pyneuroml.utils import rotate_cell
+from neuroml.utils import component_factory
+import math
+
+
+hl5doc = read_neuroml2_file("./HL5PC.cell.nml")
+hl5cell = hl5doc.cells[0]
+rotated_cell = rotate_cell(hl5cell, x=0, y=0, z=math.pi, relative_to_soma=True)
+
+newdoc = component_factory("NeuroMLDocument", id=hl5doc.id)
+newdoc.add(rotated_cell)
+
+write_neuroml2_file(newdoc, "./HL5PC.cell.rotated.nml", validate=False)
