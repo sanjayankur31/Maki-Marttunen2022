c8d96403e8289894658d991bec50efb90a88b62d
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-10-17 20:51:23 +0100

    feat(figure01): use consolidated runner

--- code/NeuroML/experiments/figure_01/figure_01_runner_HL5PC_ScZ.py
+++ code/NeuroML/experiments/figure_01/figure_01_runner_HL5PC_ScZ.py
@@ -8,79 +8,24 @@ Copyright 2022 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
-
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(".")))
 sys.path.append(os.path.dirname(os.path.abspath("..")))
 
 import numpy as np
-import matplotlib
-
-from pyneuroml.analysis import generate_current_vs_frequency_curve
-
-from figure_01_experiment import (create_model, simulate_model)
-from common import (delete_neuron_special_dir, get_run_dir, get_abs_celldir)
-
-
-# increase plot size
-matplotlib.rcParams['figure.figsize'] = [19.2, 10.8]
+from figure_01_experiment import runner
 
 
 def run():
     """Run simulations"""
-    simlist = []
-    cellname = "HL5PC"
-    celldir = get_abs_celldir("SkinnerLabHL5PC")
-    simdir = get_run_dir(cellname, "figure_01")
-    os.mkdir(simdir)
-    os.chdir(simdir)
-
-    cwd = os.getcwd()
-
-    # [1.1024124036546668, 1.128775065748801]
-    for mul_Ca_LVAst, mul_Ih in [
-            [1.0, 1.0],
-            [1.1024124036546668, 1.0],
-            [1.0, 1.128775065748801],
-            [1.1024124036546668, 1.128775065748801]
-    ]:
-        model_file_name, cell_doc_name = create_model(
-            cellname=cellname,
-            celldir=celldir,
-            current_nA="0.5 nA",
-            g_Ih_multiplier=f"{mul_Ih}",
-            g_Ca_LVAst_multiplier=f"{mul_Ca_LVAst}"
-        )
-        simlist.append(model_file_name)
-        simulate_model(model_file_name, cellname)
-
-        # delete compiled mod files
-        delete_neuron_special_dir()
-
-        generate_current_vs_frequency_curve(
-            nml2_file=cell_doc_name,
-            cell_id="HL5PC",
-            custom_amps_nA=list(np.arange(0, 150E-3, 5E-4)),
-            analysis_duration=2000,
-            analysis_delay=200,
-            temperature="34 degC",
-            simulator="jNeuroML_NEURON",
-            plot_if=True,
-            plot_iv=True,
-            pre_zero_pulse=300,
-            post_zero_pulse=300,
-            plot_voltage_traces=True,
-            save_iv_figure_to=f"{model_file_name}_iv.png",
-            save_iv_data_to=f"{model_file_name}_iv.dat",
-            save_if_figure_to=f"{model_file_name}_if.png",
-            save_if_data_to=f"{model_file_name}_if.dat",
-            save_voltage_traces_to=f"{model_file_name}_v.png",
-            show_plot_already=False,
-        )
-
-    os.chdir(cwd)
-    return simlist
+    return runner(
+        cellname="HL5PC", celldir="SkinnerLabHL5PC", num_data_points=10,
+        step_sim=True,
+        if_curve=True,
+        sim_current_na="0.5nA",
+        ifcurve_custom_amps=list(np.arange(0, 150E-3, 10E-3)),
+        scz=True)
 
 
 if __name__ == "__main__":
