36b619403bcba1d1af423d048b19c95b6c55d388
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-10-17 21:21:04 +0100

    feat(figure02): consolidate experiment runner

--- code/NeuroML/experiments/figure_02/figure_02_runner_HL5PC.py
+++ code/NeuroML/experiments/figure_02/figure_02_runner_HL5PC.py
@@ -17,57 +17,12 @@ sys.path.append(os.path.dirname(os.path.abspath("..")))
 
 
 import numpy as np
-import matplotlib
-
-from figure_02_experiment import (create_model, simulate_model,
-                                  get_segments_at_distances)
-from common import (delete_neuron_special_dir, get_abs_celldir, get_run_dir)
-
-
-# increase plot size
-matplotlib.rcParams['figure.figsize'] = [19.2, 10.8]
+from figure_02_experiment import runner
 
 
 def run():
     """Run simulations """
-    simlist = []
-    cellname = "HL5PC"
-    # run simulations in new directory so that they don't conflict with
-    # existing files.
-    celldir = get_abs_celldir("SkinnerLabHL5PC")
-    simdir = get_run_dir(cellname, "figure_02")
-
-    os.mkdir(simdir)
-    os.chdir(simdir)
-
-    dist_vs_seg_dict = get_segments_at_distances(celldir, cellname,
-                                                 "apical_dendrites_group", 50., [500])
-
-    # g: 0 is blocked, 1 is unchanged
-    # log scale starting at 10, but include 30, 100 for figure 2a
-    current_range = list(np.logspace(start=1, stop=2.5, num=20))
-    current_range.extend([30, 100])
-    current_range = sorted(current_range)
-    lems_file = ""
-    for g in [0.001, 1.0]:
-        for current in current_range:
-            model_file_name = create_model(
-                cellname=cellname,
-                celldir=celldir,
-                dist_vs_seg_dict=dist_vs_seg_dict,
-                current_nA=f"{current} nA",
-                g_Ih_multiplier=f"{g}",
-            )
-            lems_file = simulate_model(model_file_name, cellname, duration_ms=50.,
-                                       skip_run=False, plot=True)
-            simlist.append(lems_file)
-
-    # delete compiled mod files
-    delete_neuron_special_dir()
-
-    os.chdir("../")
-    print(f"Simdir: {simdir}")
-    return simlist
+    return runner(cellname="HL5PC", celldir="SkinnerLabHL5PC")
 
 
 if __name__ == "__main__":
--- code/NeuroML/experiments/figure_02/figure_02_runner_L5PC.py
+++ code/NeuroML/experiments/figure_02/figure_02_runner_L5PC.py
@@ -16,55 +16,12 @@ sys.path.append(os.path.dirname(os.path.abspath(".")))
 sys.path.append(os.path.dirname(os.path.abspath("..")))
 
 import numpy as np
-import matplotlib
-
-from figure_02_experiment import (create_model, simulate_model, get_segments_at_distances)
-from common import (delete_neuron_special_dir, get_abs_celldir,
-                    get_run_dir)
-
-
-# increase plot size
-matplotlib.rcParams['figure.figsize'] = [19.2, 10.8]
+from figure_02_experiment import runner
 
 
 def run():
     """Run simulations """
-    simlist = []
-    cellname = "L5PC"
-    # relative to simdata/figure_01/{simrun folder}
-    celldir = get_abs_celldir("HayEtAlL5PC")
-    simdir = get_run_dir(cellname, "figure_02")
-    os.mkdir(simdir)
-    os.chdir(simdir)
-
-    dist_vs_seg_dict = get_segments_at_distances(celldir, cellname,
-                                                 "apical_dendrites_group", 50., [500])
-
-    # g: 0 is blocked, 1 is unchanged
-    # log scale starting at 10, but include 30, 100 for figure 2a
-    current_range = list(np.logspace(start=1, stop=2.5, num=20))
-    current_range.extend([30, 100])
-    current_range = sorted(current_range)
-    lems_file = ""
-    for g in [0.001, 1.0]:
-        for current in current_range:
-            model_file_name = create_model(
-                cellname=cellname,
-                celldir=celldir,
-                dist_vs_seg_dict=dist_vs_seg_dict,
-                current_nA=f"{current} nA",
-                g_Ih_multiplier=f"{g}",
-            )
-            lems_file = simulate_model(model_file_name, cellname, duration_ms=50.,
-                                       skip_run=False, plot=True)
-            simlist.append(lems_file)
-
-    # delete compiled mod files
-    delete_neuron_special_dir()
-
-    os.chdir("../")
-    print(f"Simdir: {simdir}")
-    return simlist
+    return runner(cellname="L5PC", celldir="HayEtAlL5PC")
 
 
 if __name__ == "__main__":
